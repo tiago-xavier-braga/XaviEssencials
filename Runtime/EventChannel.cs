e8d293ebc61271c8a7fa4e546dcebf175b48ae2e
tiago-xavier-braga <braga.tiagoxavier@gmail.com>
2025-04-21 09:21:18 -0300

    feat-code: Improved event sending system

--- Runtime/EventChannel.cs
+++ Runtime/EventChannel.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
-using XaviEssencials.Shared;
 
 namespace XaviEssencials.Runtime
 {
-    public class EventChannel<T> : ScriptableObject
+    [CreateAssetMenu(fileName = "EventChannel", menuName = "Xavi Essencials/EventChannel")]
+    public class EventChannel : ScriptableObject
     {
-        public UnityAction<T> OnEventRaised;
+        public UnityAction OnEventRaised;
+        public UnityAction<object> OnEventRaisedWithContext;
 
-        public virtual void RaiseEvent(T value)
+        public void Raise() => OnEventRaised?.Invoke();
+
+        public void Raise(object context) => OnEventRaisedWithContext?.Invoke(context);
+
+        public void Raise(Action callback)
         {
-            OnEventRaised?.Invoke(value);
+            OnEventRaised?.Invoke();
+            callback?.Invoke();
+        }
+
+        public void Raise(object context, Action callback)
+        {
+            OnEventRaisedWithContext?.Invoke(context);
+            callback?.Invoke();
         }
     }
 }
